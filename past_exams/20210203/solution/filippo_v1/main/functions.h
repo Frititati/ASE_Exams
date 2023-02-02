2aa1afb3104f1bbb46f4e066abcfd3a1bce3a115
Filippo M Cardano <filippo1mc@gmail.com>
2023-02-02 18:48:51 +0100

    first 20210203

---
+++ past_exams/20210203/solution/filippo_v1/main/functions.h
@@ -0,0 +1,16 @@
+
+#ifndef __FUNCTIONS_H
+#define __FUNCTIONS_H
+
+#include "LPC17XX.h"
+#include "../timer/timer.h"
+
+void reset(void);
+void j_up_press(void);
+void j_left_press(void);
+void j_select_press(void);
+void update_led_value(void);
+void int0_press(void);
+
+
+#endif
---
+++ past_exams/20210203/solution/filippo_v1/main/sample.c
@@ -0,0 +1,36 @@
+
+#include "LPC17xx.h"
+#include "../timer/timer.h"
+#include "../RIT/RIT.h"
+#include "../joystick/joystick.h"
+#include "../led/led.h"
+#include "functions.h"
+
+#define SIMULATOR 1
+
+#ifdef SIMULATOR
+extern uint8_t ScaleFlag; // <- ScaleFlag needs to visible in order for the emulator to find the symbol (can be placed also inside system_LPC17xx.h but since it is RO, it needs more work)
+#endif
+
+int main(void)
+{
+  SystemInit();  												/* System Initialization (i.e., PLL)  */
+	
+	joystick_init();
+	LED_init();
+
+	init_RIT(0x004C4B40);										/* RIT inizialization to 50 ms = 0x004C4B40*/
+	
+	enable_timer(3);
+	enable_RIT();
+	
+	reset();
+	
+	LPC_SC->PCON |= 0x1;									/* power-down	mode										*/
+	LPC_SC->PCON &= ~(0x2);					
+	
+  while (1)	
+  {
+		__ASM("wfi");
+  }
+}
