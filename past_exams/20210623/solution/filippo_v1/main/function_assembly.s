336dd47414268e2a02c3f1b8e7f800bd70a02379
Filippo M Cardano <filippo1mc@gmail.com>
2023-02-21 16:58:46 +0100

    update 20210623

--- past_exams/20210623/solution/filippo_v1/main/function_assembly.s
+++ past_exams/20210623/solution/filippo_v1/main/function_assembly.s
@@ -1,56 +1,43 @@
 				AREA asm_func, CODE, READONLY
 				EXPORT sopra_la_media
 
-sopra_la_media	push {r4-r8, lr}
+sopra_la_media	PROC
 				
-				mov r4, r0   ; r0 contains the pointer to the array
-				mov r5, #0   ; initialize the sum to 0
-				mov r6, r1   ; move the length of the array into r6
-
-				ldr r7, [r0, #4]  ; load the length of the array into r7
-				add r7, r7, #1    ; add 1 to account for the length word
-				lsrs r7, r7, #2   ; divide the length by 4 to get the number of word-sized chunks
-
-sum_loop		subs r7, r7, #1   ; decrement the chunk count
-				bmi sum_done      ; exit the loop if we've processed all chunks
-
-				ldmia r4!, {r0-r3} ; load 4 elements into r0-r3
-				add r5, r5, r0    ; add r0 to the sum
-				add r5, r5, r1    ; add r1 to the sum
-				add r5, r5, r2    ; add r2 to the sum
-				add r5, r5, r3    ; add r3 to the sum
-				b sum_loop        ; repeat the loop
-
-sum_done		mov r7, r5   ; move the sum into r7
-				mov r5, #0   ; initialize the count to 0
-				ldr r8, [r0, #0]  ; load the first element into r8
+				STMFD sp!, {R4-R8, R10-R12, lr}
 				
-				mov r0, r7   ; move the sum into r0
-				mov r1, r6   ; move the length of the array into r1
+				SUB r10, r0, #1; get address of vett removing first entry
+				MOV r4, r1; index
+				MOV r6, #0; sum of array
+				MOV r8, #0;
+				
+loop_sum		SUBS r4, r4, #1
+				BMI sum_done
+				
+				LDRB r5, [r10, #1]!; get value of char in vett and update address
+				
+				ADD r6, r6, r5; add to r6 the value of vector
+				B loop_sum
+				
+sum_done		UDIV r7, r6, r1; mean
+				MOV r4, r1; reset index
+				SUB r10, r0, #1; get address of vett removing first entry
+				
+loop_over		SUBS r4, r4, #1;
+				BMI over_done
+				
+				LDRB r5, [r10, #1]!; get value of char in vett and update address
+				
+				CMP r5, r7; if gt add to counter
+				BGT over_count
+				B loop_over
+				
+over_count		ADD r8, r8, #1; add 1 to count
+				B loop_over
+				
+over_done		MOV r0, r8;
 
-divide_loop		subs r1, r1, #1   ; decrement the divisor by 1
-				bmi divide_done   ; exit the loop if we've divided by the length
-
-				sub r0, r0, r7    ; subtract the sum from the dividend
-				b divide_loop     ; repeat the loop
-
-divide_done		mov r7, r0   ; move the average into r7
-
-				add r4, r4, r6   ; add the length of the array to the pointer to get to the end of the array
-				subs r6, r6, #1  ; decrement the length by 1
-
-count_loop		subs r6, r6, #1   ; decrement the index
-				bmi count_done    ; exit the loop if we've processed all elements
-
-				ldr r8, [r4, #-1]! ; load the next element into r8
-				cmp r8, r7    ; compare the element with the average
-				bgt count_inc ; if the element is greater, increment the count
-				b count_loop  ; repeat the loop
-
-count_inc		adds r5, r5, #1 ; increment the count
-				b count_loop   ; repeat the loop
-
-count_done		mov r0, r5   ; return the count in r0
-				pop {r4-r8, pc} ; restore the saved registers and return
+				LDMFD sp!, {R4-R8, R10-R12, pc}
 				
 				END
+					
+
